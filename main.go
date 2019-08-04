85191375c792bd865292868b8149d6ce654c36d5
2393494020 <2393494020@qq.com>
2019-08-04 23:46:50 +0800

    quick sort

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main()  {
 	seed := rand.NewSource(time.Now().UnixNano())
     randGenerator := rand.New(seed)
 	
-	size := 100000
+	size := 10000
 	arr := make([]int, size, size)	
 	intSlice := make([]int, size, size)
 
@@ -21,13 +21,13 @@ func main()  {
 		intSlice[i] = arr[i]
 	}
 
-	fmt.Printf("冒泡排序前:%d\n", arr[0:10])
+	fmt.Printf("冒泡排序前:%d\n", arr[10:20])
 	sort.BubbleSort(&arr)
-	fmt.Printf("冒泡排序后:%d\n", arr[0:10])
+	fmt.Printf("冒泡排序后:%d\n", arr[10:20])
 	
-	fmt.Printf("快速排序前:%d\n", intSlice[0:10])
+	fmt.Printf("快速排序前:%d\n", intSlice[10:20])
 	sort.QuickSort(&intSlice, 0, size - 1)
-	fmt.Printf("快速排序后:%d\n", intSlice[0:10])
+	fmt.Printf("快速排序后:%d\n", intSlice[10:20])
 	
 	now := time.Now()
 	fmt.Printf("%d-%d-%d %d:%d:%d\n", now.Year(), now.Month(), now.Month(), now.Hour(), now.Minute(), now.Second())
