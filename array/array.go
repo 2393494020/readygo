f4bfc16bf25494ebfd18a784a53b0ad9a1ac0307
2393494020 <2393494020@qq.com>
2019-08-05 23:22:47 +0800

    sparse array

--- array/array.go
+++ array/array.go
@@ -9,8 +9,10 @@ func SparseArray()  {
 	chess[1][2] = 1
 	chess[2][3] = 2
 	chess[3][4] = 3
+	chess[3][5] = 3
 
 	singles := 0
+	fmt.Println("原始数组")
 	for i := 0; i < 11; i++ {
 		for j := 0; j < 11; j++ {
 			fmt.Printf("%d\t", chess[i][j])
@@ -21,12 +23,40 @@ func SparseArray()  {
 		fmt.Println()
 	}
 
-	fmt.Println(singles)
+	sparseArray := [5][3]int{}
+	sparseArray[0][0] = 11
+	sparseArray[0][1] = 11
+	sparseArray[0][2] = singles
 
-	sparseArray := [4][3]int{}
-	for i := 0; i < 4; i++ {
+	row := 1
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			if (chess[i][j] != 0) {
+				sparseArray[row][0] = i
+				sparseArray[row][1] = j
+				sparseArray[row][2] = chess[i][j]
+				row ++
+			}
+		}
+	}
+
+	fmt.Println("\n稀疏数组")
+	for i := 0; i < singles + 1; i++ {
 		for j := 0; j < 3; j++ {
 			fmt.Printf("%d\t", sparseArray[i][j])
+		}
+		fmt.Println()
+	}
+
+	chessRecovery := [ 11 ][ 11 ]int{}
+	for i := 1; i < singles + 1; i++ {
+		chessRecovery[ sparseArray[i][0] ][ sparseArray[i][1] ] = sparseArray[i][2]
+	}
+
+	fmt.Println("\n恢复后的数组")
+	for i := 0; i < sparseArray[0][0]; i++ {
+		for j := 0; j < sparseArray[0][1]; j++ {
+			fmt.Printf("%d\t", chessRecovery[i][j])
 		}
 		fmt.Println()
 	}
